c8df5809c876305170b255a95b0c753400081c32
OrchidFashion <76154898+OrchidFashion@users.noreply.github.com>
2023-08-12 20:42:38 +0530

    make changes

--- components/hero.js
+++ components/hero.js
@@ -1,37 +1,14 @@
 import "antd/dist/antd.min.css";
 import { Menu, Dropdown, Button } from "antd";
 import {
-  DownOutlined,
-  ArrowLeftOutlined,
-  ArrowRightOutlined,
-  CalendarOutlined,
-  CheckOutlined,
-  ClockCircleOutlined,
-  CloseOutlined,
-  DeleteOutlined,
-  EditOutlined,
-  ExclamationCircleOutlined,
-  HeartOutlined,
-  LeftOutlined,
-  LockOutlined,
-  MailOutlined,
-  PaperClipOutlined,
-  PhoneOutlined,
-  QuestionCircleOutlined,
-  ReloadOutlined,
-  RightOutlined,
-  SearchOutlined,
-  SendOutlined,
-  ShareAltOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+  DownOutlined} from "@ant-design/icons";
 import { useRouter } from "next/router";
 
 const Hero = () => {
   const router = useRouter();
 
   const onSearchCTAClick = () => {
-    router.push("/");
+    router.push("/properties");
   };
 
   return (
